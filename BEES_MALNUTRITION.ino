16047de7edd319eb5322302641827c733d116599
irishbitte <p.david.coughlan@gmail.com>
2017-04-30 11:40:35 +0100

    Update BEES_MALNUTRITION.ino

--- BEES_MALNUTRITION.ino
+++ BEES_MALNUTRITION.ino
@@ -59,68 +59,208 @@ void setup() {
 void loop() {
   //  prettyColourScrolling();
 
-  for(int i=1; i<13; i++)
+  for(int i=1; i<15; i++)
   {
     //call_async_threads();
-    for(int region=0; region<3; region++)
+    for(int region=0; region<6; region++)
     {
-      int density = populationDensity[region][i];
-      int red = density;
-      int green = 255 - density;
+      int beesDensity = bees[region][i];
+      int redBees = beesDensity;
+      int greenBees = 255 - beesDensity;
 
-      leds[region] = CRGB(red, green, 0);
+      leds[region*2] = CRGB(redBees, greenBees, 0);
+
+      int malDensity = malnutrition[region][i];
+      int redMalnutrition = malDensity;
+      int greenMalnutrition = 255 - malDensity;
+
+      leds[region*2+1] = CRGB(redMalnutrition, greenMalnutrition, 0);
       FastLED.show(); 
 
     }
-    delay(1000);
+    delay(600);
   }
 }
 
-void setUpPopulationDensity()
-{
-  populationDensity[PEMBROKESHIRE][12] = 245;
-  populationDensity[PEMBROKESHIRE][11] = 236;
-  populationDensity[PEMBROKESHIRE][10] = 222;
-  populationDensity[PEMBROKESHIRE][9] = 213;
-  populationDensity[PEMBROKESHIRE][8] = 204;
-  populationDensity[PEMBROKESHIRE][7] = 171;
-  populationDensity[PEMBROKESHIRE][6] = 142;
-  populationDensity[PEMBROKESHIRE][5] = 119;
-  populationDensity[PEMBROKESHIRE][4] = 102;
-  populationDensity[PEMBROKESHIRE][3] = 74;
-  populationDensity[PEMBROKESHIRE][2] = 53;
-  populationDensity[PEMBROKESHIRE][1] = 29;
-  populationDensity[BIRMINGHAM][12] = 255;
-  populationDensity[BIRMINGHAM][11] = 227;
-  populationDensity[BIRMINGHAM][10] = 193;
-  populationDensity[BIRMINGHAM][9] = 166;
-  populationDensity[BIRMINGHAM][8] = 137;
-  populationDensity[BIRMINGHAM][7] = 112;
-  populationDensity[BIRMINGHAM][6] = 92;
-  populationDensity[BIRMINGHAM][5] = 76;
-  populationDensity[BIRMINGHAM][4] = 45;
-  populationDensity[BIRMINGHAM][3] = 30;
-  populationDensity[BIRMINGHAM][2] = 15;
-  populationDensity[BIRMINGHAM][1] = 0;
-  populationDensity[EXETER][12] = 255;
-  populationDensity[EXETER][11] = 187;
-  populationDensity[EXETER][10] = 148;
-  populationDensity[EXETER][9] = 110;
-  populationDensity[EXETER][8] = 103;
-  populationDensity[EXETER][7] = 101;
-  populationDensity[EXETER][6] = 73;
-  populationDensity[EXETER][5] = 68;
-  populationDensity[EXETER][4] = 2;
-  populationDensity[EXETER][3] = 12;
-  populationDensity[EXETER][2] = 0;
-  populationDensity[EXETER][1] = 16;
-
-
-}
+// This function simply sets up the Array of Bee Data
 void setUpBees()
 {
+bees[ARMENIA][1] = 46;
+bees[ARMENIA][2] = 61;
+bees[ARMENIA][3] = 102;
+bees[ARMENIA][4] = 98;
+bees[ARMENIA][5] = 96;
+bees[ARMENIA][6] = 116;
+bees[ARMENIA][7] = 158;
+bees[ARMENIA][8] = 175;
+bees[ARMENIA][9] = 187;
+bees[ARMENIA][10] = 201;
+bees[ARMENIA][11] = 209;
+bees[ARMENIA][12] = 215;
+bees[ARMENIA][13] = 228;
+bees[ARMENIA][14] = 247;
+bees[ETHIOPIA][1] = 100;
+bees[ETHIOPIA][2] = 133;
+bees[ETHIOPIA][3] = 127;
+bees[ETHIOPIA][4] = 137;
+bees[ETHIOPIA][5] = 121;
+bees[ETHIOPIA][6] = 147;
+bees[ETHIOPIA][7] = 141;
+bees[ETHIOPIA][8] = 155;
+bees[ETHIOPIA][9] = 139;
+bees[ETHIOPIA][10] = 155;
+bees[ETHIOPIA][11] = 150;
+bees[ETHIOPIA][12] = 157;
+bees[ETHIOPIA][13] = 154;
+bees[ETHIOPIA][14] = 177;
+bees[HONDURAS][1] = 4;
+bees[HONDURAS][2] = 4;
+bees[HONDURAS][3] = 4;
+bees[HONDURAS][4] = 4;
+bees[HONDURAS][5] = 4;
+bees[HONDURAS][6] = 4;
+bees[HONDURAS][7] = 4;
+bees[HONDURAS][8] = 4;
+bees[HONDURAS][9] = 4;
+bees[HONDURAS][10] = 4;
+bees[HONDURAS][11] = 4;
+bees[HONDURAS][12] = 4;
+bees[HONDURAS][13] = 4;
+bees[HONDURAS][14] = 4;
+bees[INDIA][1] = 23;
+bees[INDIA][2] = 23;
+bees[INDIA][3] = 23;
+bees[INDIA][4] = 23;
+bees[INDIA][5] = 23;
+bees[INDIA][6] = 23;
+bees[INDIA][7] = 23;
+bees[INDIA][8] = 25;
+bees[INDIA][9] = 25;
+bees[INDIA][10] = 27;
+bees[INDIA][11] = 27;
+bees[INDIA][12] = 27;
+bees[INDIA][13] = 27;
+bees[INDIA][14] = 28;
+bees[USA][1] = 24;
+bees[USA][2] = 24;
+bees[USA][3] = 25;
+bees[USA][4] = 24;
+bees[USA][5] = 23;
+bees[USA][6] = 23;
+bees[USA][7] = 22;
+bees[USA][8] = 22;
+bees[USA][9] = 24;
+bees[USA][10] = 26;
+bees[USA][11] = 24;
+bees[USA][12] = 24;
+bees[USA][13] = 25;
+bees[USA][14] = 26;
+bees[ZAMBIA][1] = 7;
+bees[ZAMBIA][2] = 8;
+bees[ZAMBIA][3] = 9;
+bees[ZAMBIA][4] = 10;
+bees[ZAMBIA][5] = 10;
+bees[ZAMBIA][6] = 10;
+bees[ZAMBIA][7] = 10;
+bees[ZAMBIA][8] = 10;
+bees[ZAMBIA][9] = 10;
+bees[ZAMBIA][10] = 10;
+bees[ZAMBIA][11] = 11;
+bees[ZAMBIA][12] = 11;
+bees[ZAMBIA][13] = 11;
+bees[ZAMBIA][14] = 11;
+
 }
-void setUpMalnutrition();
+
+// This function sets up the malnutrition Array
+void setUpMalnutrition()
+{
+malnutrition[ARMENIA][1] = 86;
+malnutrition[ARMENIA][2] = 92;
+malnutrition[ARMENIA][3] = 89;
+malnutrition[ARMENIA][4] = 77;
+malnutrition[ARMENIA][5] = 59;
+malnutrition[ARMENIA][6] = 44;
+malnutrition[ARMENIA][7] = 33;
+malnutrition[ARMENIA][8] = 27;
+malnutrition[ARMENIA][9] = 24;
+malnutrition[ARMENIA][10] = 24;
+malnutrition[ARMENIA][11] = 26;
+malnutrition[ARMENIA][12] = 27;
+malnutrition[ARMENIA][13] = 27;
+malnutrition[ARMENIA][14] = 27;
+malnutrition[ETHIOPIA][1] = 232;
+malnutrition[ETHIOPIA][2] = 219;
+malnutrition[ETHIOPIA][3] = 208;
+malnutrition[ETHIOPIA][4] = 200;
+malnutrition[ETHIOPIA][5] = 191;
+malnutrition[ETHIOPIA][6] = 184;
+malnutrition[ETHIOPIA][7] = 175;
+malnutrition[ETHIOPIA][8] = 170;
+malnutrition[ETHIOPIA][9] = 164;
+malnutrition[ETHIOPIA][10] = 158;
+malnutrition[ETHIOPIA][11] = 150;
+malnutrition[ETHIOPIA][12] = 144;
+malnutrition[ETHIOPIA][13] = 140;
+malnutrition[ETHIOPIA][14] = 136;
+malnutrition[HONDURAS][1] = 76;
+malnutrition[HONDURAS][2] = 74;
+malnutrition[HONDURAS][3] = 71;
+malnutrition[HONDURAS][4] = 68;
+malnutrition[HONDURAS][5] = 67;
+malnutrition[HONDURAS][6] = 67;
+malnutrition[HONDURAS][7] = 66;
+malnutrition[HONDURAS][8] = 64;
+malnutrition[HONDURAS][9] = 61;
+malnutrition[HONDURAS][10] = 60;
+malnutrition[HONDURAS][11] = 60;
+malnutrition[HONDURAS][12] = 58;
+malnutrition[HONDURAS][13] = 55;
+malnutrition[HONDURAS][14] = 51;
+malnutrition[INDIA][1] = 68;
+malnutrition[INDIA][2] = 70;
+malnutrition[INDIA][3] = 74;
+malnutrition[INDIA][4] = 80;
+malnutrition[INDIA][5] = 84;
+malnutrition[INDIA][6] = 85;
+malnutrition[INDIA][7] = 82;
+malnutrition[INDIA][8] = 76;
+malnutrition[INDIA][9] = 69;
+malnutrition[INDIA][10] = 65;
+malnutrition[INDIA][11] = 63;
+malnutrition[INDIA][12] = 62;
+malnutrition[INDIA][13] = 62;
+malnutrition[INDIA][14] = 62;
+malnutrition[ZAMBIA][1] = 172;
+malnutrition[ZAMBIA][2] = 182;
+malnutrition[ZAMBIA][3] = 188;
+malnutrition[ZAMBIA][4] = 193;
+malnutrition[ZAMBIA][5] = 194;
+malnutrition[ZAMBIA][6] = 198;
+malnutrition[ZAMBIA][7] = 203;
+malnutrition[ZAMBIA][8] = 210;
+malnutrition[ZAMBIA][9] = 214;
+malnutrition[ZAMBIA][10] = 212;
+malnutrition[ZAMBIA][11] = 207;
+malnutrition[ZAMBIA][12] = 201;
+malnutrition[ZAMBIA][13] = 198;
+malnutrition[ZAMBIA][14] = 195;
+malnutrition[USA][1] = 0;
+malnutrition[USA][2] = 0;
+malnutrition[USA][3] = 0;
+malnutrition[USA][4] = 0;
+malnutrition[USA][5] = 0;
+malnutrition[USA][6] = 0;
+malnutrition[USA][7] = 0;
+malnutrition[USA][8] = 0;
+malnutrition[USA][9] = 0;
+malnutrition[USA][10] = 0;
+malnutrition[USA][11] = 0;
+malnutrition[USA][12] = 0;
+malnutrition[USA][13] = 0;
+malnutrition[USA][14] = 0;
+
+}
 
 void prettyColourScrolling()
 {
@@ -145,6 +285,11 @@ void prettyColourScrolling()
     delay(5);
   }    
 }
+
+
+
+
+
 
 
 
